b1becf1881f4a5beb8470d98c43c171e2089ab4c
Luca8991 <luca.berte98@hotmail.it>
2023-11-15 12:42:00 +0100

    chore: test docs

--- scripts/test_canister.sh
+++ scripts/test_canister.sh
@@ -4,10 +4,12 @@ set -e
 
 # unit tests
 cargo test --package ic-websocket-cdk --lib -- tests::unit_tests
+# doc tests
+cargo test --package ic-websocket-cdk --doc
 
 # integration tests
 ./scripts/download-pocket-ic.sh
 
 ./scripts/build-test-canister.sh
 
-POCKET_IC_BIN=$(pwd)/bin/pocket-ic RUST_BACKTRACE=1 cargo test --package ic-websocket-cdk --lib -- tests::integration_tests --test-threads 1
+POCKET_IC_BIN="$(pwd)/bin/pocket-ic" RUST_BACKTRACE=1 cargo test --package ic-websocket-cdk --lib -- tests::integration_tests --test-threads 1
